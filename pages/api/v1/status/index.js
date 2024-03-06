ff3ed53c6b61da72dfb3a8207163c6d6250da441
M055zilla <112419127+M055zilla@users.noreply.github.com>
2024-03-06 01:37:21 +0000

    jsconfig + npm run dev + services:up

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,12 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
   const result = await database.query("SELECT 1+1 as sum;");
   console.log(result.rows[0]);
-  res.status(200).json({});
+  res.status(200).json({
+    status: "ok",
+    data: result.rows[0],
+  });
 }
 
 export default status;
