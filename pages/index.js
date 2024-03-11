12a7fe8e6290c86ab537dcd6a1023c33f1add56e
M055zilla <112419127+M055zilla@users.noreply.github.com>
2024-03-11 04:18:30 +0000

    node-pg-migration installed and scripts created

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function home() {
-  return <h1> Criativity is inteligence having fun. </h1>;
+  return (
+    <h1>
+      {" "}
+      Criativity is inteligence having fun. <a>/status-page</a>{" "}
+    </h1>
+  );
 }
 
 export default home;
